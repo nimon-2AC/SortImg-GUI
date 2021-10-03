cf3177a8b08165570a88edb6a05f87fc3d62c948
nimon-2AC <nimon.2AC@gmail.com>
2021-10-04 01:29:45 +0900

    画像の優劣を選んでいってソートする機能を追加した

--- src/app.tsx
+++ src/app.tsx
@@ -102,13 +102,63 @@ const Entrance = () => {
   )
 }
 
+let answers: number[] = [];
 const ImageSort = () => {
-  const paths = Array.from(filePaths);
+  const [leftPath, setLeftPath] = useState("")
+  const [rightPath, setRightPath] = useState("")
 
+
+  const paths = Array.from(filePaths);
   console.log(paths);
+
+  const answer = (order: number) => {
+    answers.push(order);
+    askNext();
+  }
+  const undoAnswer = () => {
+    answers.pop();
+    askNext();
+  }
+  const askNext = () => {
+    let ai = -1;
+    let isSorted = true;
+    paths.sort((a, b) => {
+      ai++;
+      console.log(answers, ai);
+      if (ai < answers.length) {
+        return answers[ai];
+      }
+      if (ai == answers.length) {
+        console.log(answers, ai);
+        setLeftPath(a);
+        setRightPath(b);
+      }
+      isSorted = false;
+      return -1; // ここは，まだユーザーが選択してないところだから適当
+    });
+    if (isSorted) {
+      console.log("sorted!", paths);
+      console.log(answers);
+    }
+  }
+
+  useEffect(() => askNext(), [])
 
   return (
     <>
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+          <div style={{display: "flex", flexDirection: "column", flexBasis: "49%"}}>
+            <img src={leftPath} alt={leftPath} />
+            <button onClick={async () => answer(1)}>&gt;</button>
+          </div>
+          <div style={{display: "flex", flexDirection: "column", flexBasis: "49%"}}>
+            <img src={rightPath} alt={rightPath} />
+            <button onClick={async () => answer(-1)}>&lt;</button>
+          </div>
+        </div>
+        <button onClick={async () => undoAnswer()}>一手戻る</button>
+      </div>
     </>
   )
 }
@@ -118,7 +168,7 @@ const SortPage = () => {
 
   return (
     <>
-      <button onClick={() => history.goBack()}>選択をやり直す</button>
+      <button onClick={() => {answers = []; history.goBack();}}>ソート対象画像の選択をやり直す</button>
       <ImageSort />
     </>
   );
